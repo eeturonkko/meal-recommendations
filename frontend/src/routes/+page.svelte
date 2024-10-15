c5eb5609c6ee9c1094fa167bdbf7be5c8fa0327a
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-10-15 16:00:35 +0300

    add modal for adding eaten foods

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,39 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import type { Food, RecommendedFood } from '$lib/types';
-
 	export let data: PageData;
-
 	const foods: Food[] = data.foods;
 	const recommendedFoods: RecommendedFood[] = data.recommendedFoods;
+
+	let dialog: HTMLDialogElement | null;
+
+	function showModal() {
+		if (!dialog) return;
+		dialog.showModal();
+	}
+
+	function closeModal() {
+		if (!dialog) return;
+		dialog.close();
+	}
 </script>
 
 <main class="container">
 	<h1>Mitä tänään syötäisiin?</h1>
+	<button class="dialog-open-btn" on:click={showModal}>Lisää ruoka</button>
+	<dialog bind:this={dialog}>
+		<p>Lisää ruoka</p>
+		<form action="/?addFood" method="dialog">
+			<label for="food">Ruoka</label>
+			<input placeholder="Ruoka" type="text" name="food" id="food" />
+			<label for="date">Pvm</label>
+			<input type="date" name="date" id="date" />
+			<div class="dialog-form-btn-group">
+				<button type="submit">Lisää</button>
+				<button on:click={closeModal}>Peruuta</button>
+			</div>
+		</form>
+	</dialog>
 	<h2 class="text-green">Kaikki ruuat</h2>
 	<ul>
 		{#each foods as food}
