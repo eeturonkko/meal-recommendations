31d3f43c447d796b22013b6676aa390a69b29d35
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2024-10-17 19:02:55 +0300

    add ability to delete meals from client

--- frontend/src/routes/foods/+page.server.ts
+++ frontend/src/routes/foods/+page.server.ts
@@ -1,6 +1,6 @@
 import { zod } from 'sveltekit-superforms/adapters';
 import { superValidate } from 'sveltekit-superforms';
-import { getAllMeals, addMeal } from '$lib/functions';
+import { getAllMeals, addMeal, deleteMeal } from '$lib/functions';
 import { newMealFormSchema } from '$lib/formSchema';
 import type { PageServerLoad, Actions } from './$types';
 
@@ -14,5 +14,11 @@ export const actions = {
 		const form = await superValidate(event, zod(newMealFormSchema));
 		const { meal } = form.data;
 		await addMeal(meal);
+	},
+
+	deleteMeal: async (event) => {
+		const form = await superValidate(event, zod(newMealFormSchema));
+		const { id } = form.data;
+		await deleteMeal(id);
 	}
 } satisfies Actions;
